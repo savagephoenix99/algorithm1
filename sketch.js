a9b38182dda514d5ac0756a7c1cdc6cfdfb7de0b
savagephoenix99 <74053530+savagephoenix99@users.noreply.github.com>
2020-11-16 17:44:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(200, 200, 80, 50);
+  movingRect = createSprite(400, 200, 80, 50);
+  fixedRect.shapeColor="turquoise"
+  movingRect.shapeColor="turquoise"
+  movingRect.debug = true 
+  fixedRect.debug = true
+}
+
+function draw() {
+  background(255,255,255);  
+  movingRect.x= mouseX
+  movingRect.y= mouseY
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 
+    &&
+    movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2
+    &&
+    fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2 
+    &&
+    fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2)
+  {
+    fixedRect.shapeColor="green"
+    movingRect.shapeColor="green"
+  }
+  else
+  {
+    fixedRect.shapeColor="turquoise"
+    movingRect.shapeColor="turquoise"
+  }
+  console.log(movingRect.x-fixedRect.x)
+  drawSprites();
+}
